3a5b8bf0d4ff328ac142ec2a70a141ed134b6791
Volkiw <laurawlobos@gmail.com>
2025-07-22 20:15:37 +0200

    subir bd y react

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 // create and config server
-const server = express();
+const server = express(); // nombre del servidor
 server.use(cors());
 server.use(express.json());
 
@@ -11,3 +11,37 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+
+const fakeMovies = [
+  {
+    id: 1,
+    title: "Wonder Woman",
+    genre: "Action",
+    image:
+      "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
+    category: "Superhero",
+    year: 2017,
+    director: "Patty Jenkins",
+  },
+  {
+    id: 2,
+    title: "Inception",
+    genre: "Science Fiction",
+    image:
+      "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
+    category: "Thriller",
+    year: 2010,
+    director: "Christopher Nolan",
+  },
+];
+
+// server es el nombre del servidor (puede ser app, por ejemplo)
+server.get("/movies", (request, response) => {
+  response.json(
+    {
+      success: true,
+      movies:  fakeMovies
+    }
+  );
+})
