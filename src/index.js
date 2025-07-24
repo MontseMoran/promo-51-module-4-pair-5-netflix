08f4cc6a4dd68f727186a2bc6c7fd6ba2b4b9f8b
MontseMoran <m-moran@hotmail.es>
2025-07-24 16:09:00 +0200

    connection bd and front

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-
+const mysql = require('mysql2/promise')
 // create and config server
 const server = express(); // nombre del servidor
 server.use(cors());
 server.use(express.json());
+
+
+
+
 
 // init express aplication
 const serverPort = 4000;
@@ -13,35 +17,41 @@ server.listen(serverPort, () => {
 });
 
 
-const fakeMovies = [
-  {
-    id: 1,
-    title: "Wonder Woman",
-    genre: "Action",
-    image:
-      "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
-    category: "Superhero",
-    year: 2017,
-    director: "Patty Jenkins",
-  },
-  {
-    id: 2,
-    title: "Inception",
-    genre: "Science Fiction",
-    image:
-      "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
-    category: "Thriller",
-    year: 2010,
-    director: "Christopher Nolan",
-  },
-];
+  const getConnection = async () =>{
+    return await mysql.createConnection({
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.PORT
+})
+  }
+ 
+async function testConnection() {
+  const connection = await getConnection();
+  console.log(`Conexión establecida con la base de datos (identificador=${connection.threadId})`);
+  await connection.end();
+}
+
+testConnection(); 
+
+
+ 
+
+
 
 // server es el nombre del servidor (puede ser app, por ejemplo)
-server.get("/movies", (request, response) => {
-  response.json(
+server.get("/movies", async (request, response) => {
+    const connection = await getConnection();
+    const [results] = await connection.query('SELECT * FROM netflix')
+    connection.end()
+
+  response.status(200).json(
     {
       success: true,
-      movies:  fakeMovies
+      movies: results
     }
   );
+ 
 })
+
